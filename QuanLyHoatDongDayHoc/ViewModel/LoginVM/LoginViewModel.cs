cf64d12c6efa1c3cd353e1e20b024a98e22901eb
troller290 <23520867@gm.uit.edu.vn>
2024-10-28 22:44:41 +0700

    Thay đổi LoginWindow, thêm cửa sổ sign up for admin

---
+++ QuanLyHoatDongDayHoc/ViewModel/LoginVM/LoginViewModel.cs
@@ -0,0 +1,37 @@
+﻿using QuanLyHoatDongDayHoc.View.LoginWindow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace QuanLyHoatDongDayHoc.ViewModel.LoginVM
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        private string _userName;
+        public string UserName { get => _userName; set { _userName = value; OnPropertyChanged(); } }
+
+        private string _password;
+        public string Password { get => _password; set { _password = value; OnPropertyChanged(); } }
+
+
+        private bool _isLogin = false;
+        public ICommand LoginCommand { get; set; }
+        public ICommand SignUpAdminCommand { get; set; }
+
+        public LoginViewModel()
+        {
+            
+
+            SignUpAdminCommand = new RelayCommand<TextBlock>((p) => { return true; }, (p) =>
+            {
+                SignUpAdmin signUpAdmin = new SignUpAdmin();
+                signUpAdmin.ShowDialog();
+            });
+        }
+    }
+}

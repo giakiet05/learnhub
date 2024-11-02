4bc0e8f0b9d74a7ec86a79fa59cd3dd2427bb4c6
troller290 <23520867@gm.uit.edu.vn>
2024-11-02 09:59:42 +0700

    render loginview bằng loginviewmodel trên mainwindow

---
+++ LearnHub/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using LearnHub.ViewModels.AuthenticationViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnHub.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public LoginViewModel LoginVM { get; set; }
+
+        private object _currentView;
+
+		public object CurrentView
+		{
+			get { return _currentView; }
+			set { _currentView = value; OnPropertyChanged(); }
+		}
+
+        public MainViewModel()
+        {
+            LoginVM = new LoginViewModel();
+            CurrentView = LoginVM;
+        }
+    }
+}

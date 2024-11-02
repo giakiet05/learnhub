d2020795963afb3d3eaf24cfd8464d4b282aa2b6
giakiet05 <giakiet05@gmail.com>
2024-11-02 11:15:47 +0700

    tạo folder models

---
+++ LearnHub/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnHub.Models
+{
+  public class User
+    {
+        [Key]
+        public int Username { get; set; }
+    }
+}

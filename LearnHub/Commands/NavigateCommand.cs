7591e8ad8ff1a80afa8da46a6cfbff7281875c5d
Luong <luong25105@gmail.com>
2024-11-04 23:34:25 +0700

    fix bug

--- LearnHub/Commands/NavigateCommand.cs
+++ LearnHub/Commands/NavigateCommand.cs
@@ -18,7 +18,7 @@ namespace LearnHub.Commands
             _navigationStore = navigationStore;
             _createViewModel = createViewModel;
         }
-        public override void Execute(object? parameter)
+        public override void Execute(object parameter)
         {
             _navigationStore.CurrentViewModel = _createViewModel();
         }
--- LearnHub/ViewModels/AuthenticationViewModels/LoginViewModel.cs
+++ LearnHub/ViewModels/AuthenticationViewModels/LoginViewModel.cs
@@ -1,15 +1,21 @@
-﻿using LearnHub.Stores;
+﻿using LearnHub.Commands;
+using LearnHub.Stores;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace LearnHub.ViewModels.AuthenticationViewModels
 {
     public class LoginViewModel : BaseViewModel
     {
-        public LoginViewModel(NavigationStore navigationStore) { }
-        public LoginViewModel() { }
+        
+        public LoginViewModel(NavigationStore navigationStore) 
+        {
+           
+        }
+   
     }
 }

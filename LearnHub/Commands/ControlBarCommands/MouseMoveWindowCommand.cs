5bbe69966d1d3406840eadcd66d2bbbff873f37d
troller290 <23520867@gm.uit.edu.vn>
2024-11-09 16:10:11 +0700

    Thêm command close, minimize,  dragmove

---
+++ LearnHub/Commands/ControlBarCommands/MouseMoveWindowCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LearnHub.Commands.ControlBarCommands
+{
+    public class MouseMoveWindowCommand : BaseCommand
+    {
+        public override void Execute(object parameter)
+        {
+            FrameworkElement parent = GetParent(parameter as UserControl);
+            var w = parent as Window;
+            if (w != null)
+                w.DragMove();
+        }
+        FrameworkElement GetParent(UserControl p)
+        {
+            FrameworkElement parent = p;
+            while (parent.Parent != null)
+            {
+                parent = parent.Parent as FrameworkElement;
+            }
+            return parent;
+        }
+    }
+}

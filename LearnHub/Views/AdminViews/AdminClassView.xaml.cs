dc20a4f5f0b38b9274b7bf9d6bed966d1d53be3f
trananhduck <anhduc9b1cva@gmail.com>
2024-11-12 10:45:04 +0700

    binding data for adminClassView

--- LearnHub/Views/AdminViews/AdminClassView.xaml.cs
+++ LearnHub/Views/AdminViews/AdminClassView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LearnHub.ViewModels.AdminViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace LearnHub.Views.AdminViews
         public AdminClassView()
         {
             InitializeComponent();
+            DataContext = new AdminClassViewModel();
         }
     }
 }

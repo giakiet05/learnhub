466f5ecafcda8517a1c9868421ed82bfd80c4c04
troller290 <23520867@gm.uit.edu.vn>
2024-11-21 08:51:18 +0700

    thêm view bộ môn, sửa các view liên quan

---
+++ LearnHub/Views/AdminViews/AdminMajorView.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LearnHub.Views.AdminViews
+{
+    /// <summary>
+    /// Interaction logic for AdminMajorView.xaml
+    /// </summary>
+    public partial class AdminMajorView : UserControl
+    {
+        public AdminMajorView()
+        {
+            InitializeComponent();
+        }
+        private void BoMonDG_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var dataGrid = sender as DataGrid;
+
+            // Lấy đối tượng nơi nhấn chuột
+            var hit = VisualTreeHelper.HitTest(dataGrid, e.GetPosition(dataGrid));
+
+            if (hit != null)
+            {
+                // Xác định dòng được nhấn (nếu có)
+                var row = ItemsControl.ContainerFromElement(dataGrid, hit.VisualHit) as DataGridRow;
+
+                if (row != null)
+                {
+                    // Chọn dòng
+                    dataGrid.SelectedItem = row.Item;
+                }
+                else
+                {
+                    // Nếu nhấn vào khoảng trống, không thay đổi lựa chọn
+                    e.Handled = true;
+                }
+            }
+        }
+    }
+}
